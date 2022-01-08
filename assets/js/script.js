f5f560b0f156c0e4ad310454f0228a566aa40487
ckyle121 <carolinekyle21@gmail.com>
2022-01-08 12:55:58 -0600

    add custom css elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,7 +156,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -198,9 +198,11 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event){
     $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event){
     $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event){
     $(event.target).addClass("dropover-active");
@@ -248,12 +250,14 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui){
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui){
-    console.log("over");
+    console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui){
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
